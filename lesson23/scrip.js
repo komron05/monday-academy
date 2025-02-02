60da6ff31fcbc66c8a1d7b41ab4f42503757aae7
Melikulov Komronbek <komronbekmelikulov0@gmail.com>
2025-02-02 12:31:50 +0800

    box and loesson23

---
+++ lesson23/scrip.js
@@ -0,0 +1,38 @@
+// let rang = document.querySelector('.rang')
+
+// function changeColor(color){
+//     rang.id(color)
+//    }
+
+// rang.addEventListener('mouseenter',()=>{
+//     changeColor('yellows')
+// })
+     
+let rang = document.querySelector('.rang')
+let button = document.querySelector('#bosing')
+let isRounded = rang.classList.contains('rounded')
+
+function changeColor(color){
+ rang.id=color
+}
+
+rang.addEventListener('mouseenter',()=>{
+changeColor('yellows')
+})
+
+rang.addEventListener('mouseleave',()=>{
+changeColor('reds')
+setTimeout(function(){
+rang.id=''
+},5000)
+});
+
+button.addEventListener('click',()=>{
+    if(isRounded){
+        rang.classList.remove('rounded')
+        isRounded=!isRounded
+    }else{
+        rang.classList.add('rounded')
+        isRounded=!isRounded
+    }
+})
